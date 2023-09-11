6185071930fda20960655d63ff40e8e8f0c97ec5
SayWes <whesley09s@gmail.com>
2023-09-11 10:53:47 -0300

    feat(js): funçoes anonimas e numeros randomicos

---
+++ advanced level/first project/src/js/script.js
@@ -0,0 +1,36 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btntry");
+const btnReset = document.querySelector("#btnReset");
+
+const randomNumber = Math.round(Math.random() * 10);
+
+//event
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click");
+
+//funçao callback
+const handleTryClick = (event) => {
+  event.preventDefault(); //nao faca o padrao
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+
+    document.querySelector(
+      ".screen2 h2"
+    ).innerHTML = `acertou em ${xAttempts} tentativas`;
+  }
+  inputNumber.value = "";
+  xAttempts++;
+};
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
