ff8cbc4fed49ebfadef520449308cf70d47e9fe6
SayWes <whesley09s@gmail.com>
2023-09-19 16:57:36 -0300

    feat(js): advanced on secund project IMC and understand ES6 module

---
+++ advanced level/Es Module/main.js
@@ -0,0 +1,3 @@
+import message from "./utils"; // esta importando e ja executando o arquivo
+
+console.log(message);
---
+++ advanced level/Es Module/utils.js
@@ -0,0 +1,4 @@
+const message = "mensagem"; // esta isolada nesse arquivo
+
+// a const message ta isolada no escopo do arquivo utils
+export default message;
